2337c93a58763d5871d090a066b24a64b5dc0cf3
Huahuan Zheng <maxwellzh@outlook.com>
2021-12-21 01:14:30 +0800

    modify setuptools

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # CUDA-Warp RNN-Transducer loss for compact layout input
+
+```
+# install
+pip install -r requirements.txt
+
+# install for development
+python setup.py develop
+```
--- setup.py
+++ setup.py
@@ -6,12 +6,6 @@ from setuptools import setup, find_packages
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 
-def get_requirements():
-    req_file = os.path.join(os.path.dirname(__file__), "requirements.txt")
-    with open(req_file, "r") as f:
-        return [line.strip() for line in f]
-
-
 def get_long_description():
     readme_file = os.path.join(os.path.dirname(__file__), "README.md")
     with open(readme_file, "r") as f:
@@ -22,7 +16,6 @@ if not torch.cuda.is_available():
     raise Exception("CPU version is not implemented")
 
 
-requirements = get_requirements()
 long_description = get_long_description()
 
 setup(
@@ -38,10 +31,13 @@ setup(
             sources=["cwarp_cuda.cpp", "cwarp_cuda_gather_kernel.cu",
                      "cwarp_cuda_rnnt_kernel.cu"],
             extra_compile_args={
-                'nvcc': ["-arch=sm_60"]}
+                'nvcc': [
+                    "-gencode=arch=compute_60,code=sm_60",
+                    "-gencode=arch=compute_61,code=sm_61",
+                    "-gencode=arch=compute_70,code=sm_70",
+                    "-gencode=arch=compute_80,code=sm_80",
+                    "-gencode=arch=compute_86,code=sm_86"]}
         )
     ],
-    cmdclass={"build_ext": BuildExtension},
-    setup_requires=requirements,
-    install_requires=requirements
+    cmdclass={"build_ext": BuildExtension}
 )
